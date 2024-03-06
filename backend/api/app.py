8cd3f08b6c2f03e8f1a322e7f0844450e51cdc8f
Umair <umair98392@gmail.com>
2024-03-07 00:23:30 +0530

    final

--- backend/api/app.py
+++ backend/api/app.py
@@ -113,4 +113,4 @@ async def predict(file: UploadFile):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
