da5b38e667320566ba32ba4e0d924793704f1edc
Umair <umair98392@gmail.com>
2024-03-11 18:40:31 +0530

    11 Mar

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-from api.cric import extract_keypoints, result_images
+from api.cric import extract_keypoints, result_images, prediction_on_video
 import joblib
-import torch
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow.keras.models import load_model
 import numpy as np
 import cv2
-import os
 import base64
+import warnings
+warnings.filterwarnings("ignore")
+    
 
 
 app = FastAPI()
@@ -39,11 +44,12 @@ app.add_middleware(
 output_directory = './output_directory'        # Replace with the actual directory path
 
 # Load the pre-trained model and scaler to transform keypoints
-model = torch.load('models/trained_model/model.pth')
-loaded_scaler = joblib.load('models/trained_model/scaler.pkl')
+model = load_model('./models/trained_model/i_model.keras')
+loaded_scaler = joblib.load('./models/trained_model/i_scaler.pkl')
+v_model = load_model('./models/trained_model/v_model.keras')
 
 # Define the desired resolution
-desired_resolution = (240, 240)
+desired_resolution = (288, 288)
 
 # Home page route
 @app.get("/")
@@ -59,10 +65,14 @@ async def predict(file: UploadFile):
     nparr = np.frombuffer(content, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Resize the image to the desired resolution
-    img = cv2.resize(img, desired_resolution)
+    img = cv2.resize(img, desired_resolution,interpolation=cv2.INTER_LINEAR)
+    
     # call function to extract keypoint
     kpp = extract_keypoints(img)
+
+    if len(kpp) ==0:
+        return              #Error for valid image
+    
 
     # Call function to create images with box and keypoints detection
     result_images(img)
@@ -71,15 +81,13 @@ async def predict(file: UploadFile):
     kpp = loaded_scaler.transform(kpp.reshape(1, -1))
     kpp = np.array(kpp)
 
-    # convert into tensor to accept by pytorch model
-    x_t = torch.Tensor(kpp)
 
     # get prediction
-    pred = model(x_t)
-    predicted = np.argmax(pred.cpu().data.numpy(), axis=1)
-    shot_types = ['Drive', 'Legglance-Flick', 'Pullshot', 'Sweep']
+    pred = model(kpp)
+    predicted = np.argmax(pred, axis=1)
+    shot_types = ['Cut-Shot', 'Drive', 'Legglance-Flick', 'Pullshot', 'Sweep']
     predicted_shot = shot_types[predicted[0]]
-    confidence = float(np.max(pred.cpu().data.numpy()))*100
+    confidence = float(np.max(pred))*100
 
     # Check if the output directory exists
     if not os.path.exists(output_directory):
@@ -111,6 +119,44 @@ async def predict(file: UploadFile):
     return response_json
 
 
+
+@app.post("/predict_video")
+async def predict_video(file: UploadFile):
+    # Read and process the uploaded video using OpenCV
+    content = await file.read()
+    # Save the video file temporarily or process it in memory depending on your requirements
+    # Example: Save the file to disk
+    with open("temp_video.mp4", "wb") as video_file:
+        video_file.write(content)
+    
+    # Perform video analysis and prediction
+    result_json,predicted_video_path = prediction_on_video(model=v_model,path="temp_video.mp4")
+    
+    # /video_path = "test1.mp4"
+    # Read the predicted video content as bytes
+    with open(predicted_video_path, "rb") as video_file:
+        video_content = video_file.read()
+
+    # Encode the video content as base64
+    video_base64 = base64.b64encode(video_content).decode("utf-8")
+
+    # Clean up the temporary video file
+    os.remove("temp_video.mp4")
+
+    # Include the base64-encoded video in the JSON response
+    result_json["predicted_video"] = video_base64
+
+    return result_json
+
+# video_path = 'predicted_result.mp4'
+# @app.get("/video")
+# async def get_video():
+#     return StreamingResponse(open(video_path, "rb"), media_type="video/mp4")
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
