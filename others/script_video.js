44410acd518a7f06da28d86c71839bb81c75e753
Mohd Umair <umair98392@gmail.com>
2024-04-21 20:51:26 +0530

    final com

--- others/script_video.js
+++ others/script_video.js
@@ -1,50 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const imageUpload = document.getElementById("image-upload");
+    const videoUpload = document.getElementById("video-upload");
     const predictButton = document.getElementById("predict-button");
     const predictedAge = document.getElementById("predicted-age");
     const predictedGender = document.getElementById("predicted-gender");
-    const uploadedImage = document.getElementById("uploaded-image");
-    const predictedImages = document.querySelectorAll(".predicted-image");
+    const uploadedVideo = document.getElementById("uploaded-video");
+    const resultVideo = document.getElementById("result-video");
     const loader = document.querySelector("#loader");
 
     // Event listener for the predict button
     predictButton.addEventListener("click", async function () {
-        
-        const fileInput = imageUpload.files[0];
+        const fileInput = videoUpload.files[0];
 
         if (fileInput) {
-            
-            //image to show on webpage
-            uploadedImage.src = URL.createObjectURL(fileInput);
-            
-            // Create a FormData object to send the image file
+            // Video to show on webpage
+            uploadedVideo.src = URL.createObjectURL(fileInput);
+
+            // Create a FormData object to send the video file
             const formData = new FormData();
             formData.append("file", fileInput);
-
-            
 
             try {
                 // Show loader
                 loader.style.display = "block";
 
                 // Send a POST request to your FastAPI backend
-                const response = await fetch("https://noble-gently-barnacle.ngrok-free.app/predict", { method: "POST",body: formData});
+                // "https://sturgeon-light-especially.ngrok-free.app/predict_video"
+                //https://cricket-posenet-ai-shot-recognition.onrender.com/predict_video
+                
+                
+                const response = await fetch("https://noble-gently-barnacle.ngrok-free.app/predict_video", { method: "POST", body: formData });
 
                 if (response.ok) {
                     // Parse the JSON response
                     const data = await response.json();
-                    
+
                     // Update the prediction result in the HTML
                     console.log(data);
-                    console.log(data.confidence);
-                    predictedAge.textContent =  data.confidence
+                    predictedAge.textContent = data.confidence;
                     predictedGender.textContent = data.predicted_shot;
 
-                    // Update predicted images
-                    for (let i = 0; i < predictedImages.length; i++) {
-                        predictedImages[i].src = `data:image/png;base64,${data[`result_image_${i + 1}`]}`;
-                    }
-                
+                    // Update result video
+                    resultVideo.src = `data:video/mp4;base64,${data.predicted_video}`;
                 } else {
                     console.error("Error:", response.statusText);
                 }
@@ -52,11 +48,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error("Error:", error);
             }
 
-
-        // Hide loader after response
-        loader.style.display = "none";
-
-        } else 
-            alert("Please select an image to predict.");
+            // Hide loader after response
+            loader.style.display = "none";
+        } else {
+            alert("Please select a video to predict.");
+        }
     });
 });
